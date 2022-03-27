431e1aae730eb0463d9d5bcb0a0420a5620e38e1
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-03-27 10:28:10 +0100

    Adds order filler testing

--- activity/order_filler_test.go
+++ activity/order_filler_test.go
@@ -4,10 +4,123 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/MihaiBlebea/trading-platform/account"
+	"github.com/MihaiBlebea/trading-platform/activity"
+	"github.com/MihaiBlebea/trading-platform/order"
+	"github.com/MihaiBlebea/trading-platform/pos"
 	"github.com/sirupsen/logrus"
 )
 
 func TestCanFillBuyOrder(t *testing.T) {
 	logger := logrus.New()
 	logger.Out = ioutil.Discard
+
+	accountRepo := account.AccountRepoMock{}
+	account, _ := accountRepo.Save(account.NewAccount())
+
+	orderRepo := order.OrderRepoMock{}
+	posRepo := pos.NewPositionRepoMock()
+
+	amount := float32(1000.00)
+	symbol := "aapl"
+	o, err := orderRepo.Save(order.NewBuyOrder(
+		account.ID,
+		"limit",
+		symbol,
+		1000.00,
+	))
+	if err != nil {
+		t.Errorf("error message: %s", err)
+		return
+	}
+
+	if o.Amount != amount {
+		t.Errorf("expected order amount %v, got %v", amount, o.Amount)
+	}
+
+	orderFiller := activity.NewFiller(
+		&accountRepo,
+		&orderRepo,
+		posRepo,
+		logger,
+	)
+
+	if err := orderFiller.FillPendingOrders(); err != nil {
+		t.Errorf("error message: %s", err)
+		return
+	}
+
+	o, err = orderRepo.WithId(o.ID)
+	if err != nil {
+		t.Errorf("error message: %s", err)
+		return
+	}
+
+	if o.Status != order.StatusFilled {
+		t.Errorf(
+			"expected order amount %v, got %v",
+			order.StatusFilled,
+			o.Status,
+		)
+	}
+}
+
+func TestCanFillSellOrder(t *testing.T) {
+	logger := logrus.New()
+	logger.Out = ioutil.Discard
+
+	accountRepo := account.AccountRepoMock{}
+	account, _ := accountRepo.Save(account.NewAccount())
+
+	orderRepo := order.OrderRepoMock{}
+	posRepo := pos.NewPositionRepoMock()
+
+	symbol := "aapl"
+	quantity := 50
+	posRepo.Save(pos.NewPosition(account.ID, symbol, quantity))
+
+	o, err := orderRepo.Save(order.NewSellOrder(
+		account.ID,
+		"limit",
+		symbol,
+		quantity,
+	))
+	if err != nil {
+		t.Errorf("error message: %s", err)
+		return
+	}
+
+	if o.Quantity != quantity {
+		t.Errorf(
+			"expected order quantity %v, got %v",
+			quantity,
+			o.Quantity,
+		)
+	}
+
+	orderFiller := activity.NewFiller(
+		&accountRepo,
+		&orderRepo,
+		posRepo,
+		logger,
+	)
+
+	if err := orderFiller.FillPendingOrders(); err != nil {
+		t.Errorf("error message: %s", err)
+		return
+	}
+
+	o, err = orderRepo.WithId(o.ID)
+	if err != nil {
+		t.Errorf("error message: %s", err)
+		return
+	}
+
+	if o.Status != order.StatusFilled {
+		t.Errorf(
+			"expected order amount %v, got %v",
+			order.StatusFilled,
+			o.Status,
+		)
+	}
 }
