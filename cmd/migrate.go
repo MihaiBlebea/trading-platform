9d095bc8fc325ae8593dcabe87f0c1e0f2e171ba
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-11 12:21:33 +0100

    Adds drop table command

---
+++ cmd/migrate.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/MihaiBlebea/trading-platform/di"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(migrateCmd)
+}
+
+var migrateCmd = &cobra.Command{
+	Use: "drop-table",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			return errors.New("provide at least one argument for the table name")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		tableName := args[0]
+
+		di := di.NewContainer()
+		conn, err := di.GetDatabaseConn()
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Dropping table %s \n", tableName)
+		conn.Migrator().DropTable(tableName)
+
+		return nil
+	},
+}
